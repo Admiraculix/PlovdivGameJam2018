d98e601488dfa862fbd99f5a548a6f9d49317eb8
Georgi Bozhilov <talic@abv.bg>
2018-01-27 20:18:11 +0200

    Added new Scripts/SpinningBoltScript.cs

---
+++ Greasy_PGJ2018/Assets/Scripts/SpinningBoltScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningBoltScript : MonoBehaviour {
+
+    float randomTimeInterval;
+    float randomSpinDegrees;
+    Rigidbody2D objectRigidBody;
+
+    private void Start()
+    {
+        objectRigidBody = GetComponent<Rigidbody2D>();
+        randomTimeInterval = Random.Range(2.0f, 6.0f);
+        randomSpinDegrees = Random.Range(0.0f, 360.0f);
+        StartCoroutine(SpinAndWait());
+    }
+
+    IEnumerator SpinAndWait()
+    {
+        yield return new WaitForSecondsRealtime(randomTimeInterval);
+
+        randomTimeInterval = Random.Range(2.0f, 6.0f);
+        randomSpinDegrees = Random.Range(0.0f, 360.0f);
+        StartCoroutine(SpinAndWait());
+    }
+
+    private void Update()
+    {
+        objectRigidBody.MoveRotation(randomSpinDegrees);
+    }
+}
