9f56443bba2d19d5f84cd6b787c43d8321f685a5
Georgi Bozhilov <talic@abv.bg>
2018-01-28 12:29:01 +0200

    New Features is added

---
+++ Greasy_PGJ2018/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour {
+
+    public AudioClip loopMusic;
+    AudioSource audioSRX;
+
+	void Start () {
+        audioSRX = GetComponent<AudioSource>();
+        audioSRX.clip = loopMusic;
+        audioSRX.loop = true;
+        audioSRX.Play();
+    }
+}
---
+++ Greasy_PGJ2018/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public AudioClip loopMusic;
+    AudioSource audioSRX;
+
+    private void Start()
+    {
+        audioSRX = GetComponent<AudioSource>();
+        audioSRX.clip = loopMusic;
+        audioSRX.loop = true;
+        audioSRX.Play();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void PlayDemo()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void PlayCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
